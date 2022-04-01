030bc36f0d844e2f5058377223b0de1d49467fdd
P22inolan <p22inolan@fairfieldprep.org>
2022-04-01 10:20:21 -0400

    WIP 4/1

--- ian_work.py
+++ ian_work.py
@@ -19,7 +19,6 @@ with open("/Users/p22inolan/Desktop/project/turtle-project-code-ian-colin/leader
     if first_line == "\n":
         leaderboard.write("INITIALS     SCORE")
 
-
 head = turtle.Turtle()
 head.shape("square")
 color = turtle.textinput("Color", "What color should the snake be?")
@@ -42,27 +41,34 @@ food.speed(0)
 food.penup()
 food.goto(0, 200)
 
-score = turtle.Turtle()
-score.speed(0)
-score.shape("square")
-score.color("white")
-score.penup()
-score.hideturtle()
-score.goto(0, 325)
-score.write("Score: 0  High Score: 0", align="center", font=("Comic Sans", 24, "bold"))
+scoreboard = turtle.Turtle()
+scoreboard.speed(0)
+scoreboard.shape("square")
+scoreboard.color("white")
+scoreboard.penup()
+scoreboard.hideturtle()
+scoreboard.goto(0, 270)
+scoreboard.write("Score: 0  High Score: 0", align="center", font=("Comic Sans", 24, "bold"))
 
 def snake_up():
     if head.heading != 270:
         head.heading = 90
+
+
 def snake_down():
     if head.heading != 90:
         head.heading = 270
+
+
 def snake_left():
     if head.heading != 0:
         head.heading = 180
+
+
 def snake_right():
     if head.heading != 180:
         head.heading = 0
+
 
 def movement():
     if head.heading == 90:
@@ -78,6 +84,7 @@ def movement():
         x = head.xcor()
         head.setx(x+20)
 
+
 window.listen()
 window.onkeypress(snake_up, "w")
 window.onkeypress(snake_down, "s")
@@ -86,6 +93,10 @@ window.onkeypress(snake_right, "d")
 
 body = []
 
+#play = window.textinput("Ready?", "Ready to play? (Y/N)")
+#play.isupper()
+
+#while play == "Y":
 while True:
     window.update()
     if head.xcor() > 280 or head.xcor() < -280 or head.ycor() > 280 or head.ycor() < -280:
@@ -93,12 +104,67 @@ while True:
         time.sleep(1)
         head.penup
         head.goto(0,0)
+        for segment in body:
+            segment.goto(1000, 1000)
+        body.clear()
+        scoreboard.clear()
+        scoreboard.write("Score: {0}  High Score: {1}".format(score, high_score), align="center", font=("Comic Sans", 24, "bold"))
         initials = window.textinput("Game Over!", "Enter your initials:")
         leaderboard.write("\n {0}         {1}".format(initials, score))
+        play_again = window.textinput("Game Over!", "Do you want to play again? (Y/N)")
+        play_again.isupper()
         turtle.bye()
-    for segment in segments:
-        segment.goto(1000, 1000)
     
+    if head.distance(food) < 20:
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 270)
+        food.goto(x, y)
+    
+        tail = turtle.Turtle()
+        tail.speed(0)
+        tail.shape("square")
+        tail.color(color)
+        tail.penup()
+        body.append(tail)
+        score += 10
+        if score > high_score:
+            high_score = score
+        scoreboard.clear()
+        scoreboard.write("Score: {0}  High Score: {1}".format(score, high_score), align="center", font=("Comic Sans", 24, "bold"))
+
+    for index in range(len(body)-1, 0, -1):
+        x = body[index-1].xcor()
+        y = body[index-1].ycor()
+        body[index].goto(x, y)
+    if len(body) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        body[0].goto(x, y)
     movement()
+    for segment in body:
+        if segment.distance(head) < 20:
+            head.color("red")
+            time.sleep(1)
+            head.penup
+            head.goto(0,0)
+            for segment in body:
+                segment.goto(1000, 1000)
+            segment.clear()
+            scoreboard.clear()
+            scoreboard.write("Score: {0}  High Score: {1}".format(score, high_score), align="center", font=("Comic Sans", 24, "bold"))
+            initials = window.textinput("Game Over!", "Enter your initials:")
+            leaderboard.write("\n {0}         {1}".format(initials, score))
+    #play_again = window.textinput("Try Again?", "Do you want to play again? (Y/N)")
+    #play_again.isupper
+
+   # if play_again == "N":
+    #    turtle.bye()
+   # if play == "N":
+   #     turtle.bye()
+   # elif play != "Y" or "N":
+    #    error = window.textinput("Invalid", "Please enter either Y or N. Type OK to continue.")
+    #    error.isupper()
+    #    if error == "OK":
+    #        play
 
 window.mainloop()
